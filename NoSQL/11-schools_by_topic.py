49e96489a616037ed72eb4159ab0e3c0f734364b
elvinmammadzada <elvinmammadzada643@gmail.com>
2025-11-29 20:12:36 +0000

    S

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+11-schools_by_topic.py
+Return the list of schools having a specific topic
+"""
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    mongo_collection: pymongo collection object
+    topic: string
+    Return: list of schools containing topic in their 'topics' field
+    """
+    return list(mongo_collection.find({"topics": topic}))
